96739819d753a59f60b0bab808b5b4ff7381dc89
juniorLeon1 <leon71314@gmail.com>
2023-02-28 13:18:58 -0500

    uploaded testing folder

---
+++ testing/app.js
@@ -0,0 +1,31 @@
+const request = require('request');
+
+const inputText = 'dang'
+const langFrom = 'en'
+const langTo = 'es'
+
+const options = {
+  method: 'POST',
+  url: 'https://rapid-translate-multi-traduction.p.rapidapi.com/t',
+  headers: {
+    'content-type': 'application/json',
+    'X-RapidAPI-Key': '54523f2d11msh4eaa24da65900c6p1f3717jsn48bde874ef56',
+    'X-RapidAPI-Host': 'rapid-translate-multi-traduction.p.rapidapi.com',
+    useQueryString: true
+  },
+  body: {
+    from: langFrom,
+    to: langTo,
+    e: '',
+    q: [
+      inputText
+    ]
+  },
+  json: true
+};
+
+request(options, function (error, response, body) {
+	if (error) throw new Error(error);
+
+	console.log(body);
+});
